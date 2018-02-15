7f23026d03fb74b83221fa14bddcc384d1329d5b
t03apt <t03apt@hotmail.com>
2018-02-15 23:13:55 +0100

    add(GetDocumentDatabasesCmdlet)

---
+++ PsCmdletTest/GetDocumentDatabasesCmdlet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Management.Automation;
+using Microsoft.Azure.Documents.Client;
+
+namespace SendGreeting
+{
+    [Cmdlet(VerbsCommon.Get, "DocumentDatabases")]
+    public class GetDocumentDatabasesCmdlet : Cmdlet
+    {
+        [Parameter(Mandatory = true)]
+        public string ServiceUrl { get; set; }
+
+        [Parameter(Mandatory = true)]
+        public string AuthKey { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            try
+            {
+                DocumentClient client = new DocumentClient(new Uri(ServiceUrl), AuthKey);
+                var result = client.ReadDatabaseFeedAsync();
+
+                WriteObject(result);
+            }
+            catch (Exception ex)
+            {
+                WriteObject(ex);
+            }
+        }
+    }
+}
